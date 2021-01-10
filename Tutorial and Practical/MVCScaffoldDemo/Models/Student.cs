8c4b63d08536a0f8ed41ae5c1893a0d15f231bd9
Hem Bhagat <hembhagat99@gmail.com>
2021-01-10 20:36:47 +0530

    Tutorial and Practical Added

---
+++ Tutorial and Practical/MVCScaffoldDemo/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCScaffoldDemo.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Enrollment { get; set; }
+
+        public int Age { get; set; }
+    }
+
+    public class StudentDBContext : DbContext
+    {
+        public StudentDBContext() { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
