de2a05bafc902f681fc2f2ed2a74926bee0ddffc
Hem Bhagat <hembhagat99@gmail.com>
2021-02-11 16:58:49 +0530

    Progressed in dot NET Final Assignment

---
+++ dot NET Final Assignment/Admin/Controllers/MechnicController.cs
@@ -0,0 +1,41 @@
+﻿using Business.Interface;
+using BusinessEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Controllers
+{
+    public class MechnicController : Controller
+    {
+        private IMechnicManager _mechanicManager;
+
+        public MechnicController(IMechnicManager mechanicManager)
+        {
+            _mechanicManager = mechanicManager;
+        }
+
+        // GET: Mechnic
+        public ActionResult Index()
+        {
+            var mechanics = _mechanicManager.GetMechanics();
+            return View(mechanics);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(MechanicBusinessEntity businessEntity)
+        {
+            businessEntity.Number = Guid.NewGuid();
+            _mechanicManager.AddMechanic(businessEntity);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ dot NET Final Assignment/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,44 @@
+﻿using Business.Interface;
+using BusinessEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private IServiceManager _serviceManager;
+
+        public ServiceController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        // GET: Service
+        public ActionResult Index(int? mechID)
+        {
+            var services = _serviceManager.GetServices().Where(x => x.MechanicID == mechID);
+            return View(services);
+        }
+
+        public ActionResult Create(int mechID)
+        {
+            TempData["mech"] = mechID;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ServiceBusinessEntity businessEntity)
+        {
+            businessEntity.Number = Guid.NewGuid();
+            businessEntity.MechanicID = Convert.ToInt32(TempData["mech"]);
+            businessEntity.IsActive = true;
+            _serviceManager.AddService(businessEntity);
+            return RedirectToAction("Index","Mechnic");
+        }
+    }
+}
