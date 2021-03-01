3aba3489cf9c98b8fd82229b0a657c1811f4c3fc
Hem Bhagat <hembhagat99@gmail.com>
2021-03-01 18:12:02 +0530

    Added 3 extension methods in Testing Final Assignment 2

--- Testing Final Assignment 2/Program.cs
+++ Testing Final Assignment 2/Program.cs
@@ -10,8 +10,41 @@ namespace Testing_Final_Assignment_2
     {
         public static string StringExtension(this string input, string operation)
         {
+            string output = "";
+            int ascii;
             if (operation == "inverse")
-                return "string inversed";
+            {
+                foreach(char ch in input)
+                {
+                    ascii = (int)ch;
+                    if (ascii >= 65 && ascii <= 90)
+                        ascii += 32;
+                    else if (ascii >= 97 && ascii <= 122)
+                        ascii -= 32;
+                    output += (char)ascii;
+                }
+                return output;
+            }
+            else if(operation == "words")
+            {
+                return input.Split(' ').Length.ToString();
+            }
+            else if(operation == "isLowerCase")
+            {
+                bool isLowerCase = true;
+                foreach(char ch in input)
+                {
+                    ascii = (int)ch;
+                    if (ch == ' ')
+                        continue;
+                    if(ascii <= 97 && ascii >= 122)
+                    {
+                        isLowerCase = false;
+                        break;
+                    }
+                }
+                return isLowerCase ? "yes" : "no";
+            }
             else
                 return "default";
         }
@@ -20,6 +53,17 @@ namespace Testing_Final_Assignment_2
     {
         static void Main(string[] args)
         {
+            string input = "Hello World";
+            string output = input.StringExtension("inverse");
+            Console.WriteLine("Inverse operation: Hello World => " + output);
+            output = input.StringExtension("words");
+            Console.WriteLine("Number of Words: Hello World => " + output);
+            output = input.StringExtension("isLowerCase");
+            Console.WriteLine("IsLowerCase: Hello World => " + output);
+            input = "hello world";
+            output = input.StringExtension("isLowerCase");
+            Console.WriteLine("IsLowerCase: hello world => " + output);
+            Console.ReadKey();
         }
     }
 }
