7895ecbed9c09d9b116c30cdc77427f4fd38b5d9
Hem Bhagat <hembhagat99@gmail.com>
2021-03-01 17:43:18 +0530

    Testing Final Assignment 2 added

---
+++ Testing Final Assignment 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_Final_Assignment_2
+{
+    public static class ExtensionMethods
+    {
+        public static string StringExtension(this string input, string operation)
+        {
+            if (operation == "inverse")
+                return "string inversed";
+            else
+                return "default";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
