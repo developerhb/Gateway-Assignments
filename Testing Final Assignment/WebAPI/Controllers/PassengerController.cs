d7c0d63f96fb57fb196c4ba67ea6970dc1a705c8
Hem Bhagat <hembhagat99@gmail.com>
2021-03-02 20:28:09 +0530

    Testing Final Assignment

---
+++ Testing Final Assignment/WebAPI/Controllers/PassengerController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class PassengerController : ApiController
+    {
+        private DBContext db = new DBContext();
+
+        // GET: api/Passenger
+        public IQueryable<Passenger> GetPassenger()
+        {
+            return db.Passenger;
+        }
+
+        // GET: api/Passenger/5
+        [ResponseType(typeof(Passenger))]
+        public IHttpActionResult GetPassenger(int id)
+        {
+            Passenger passenger = db.Passenger.Find(id);
+            if (passenger == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(passenger);
+        }
+
+        // PUT: api/Passenger/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPassenger(int id, Passenger passenger)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != passenger.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(passenger).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PassengerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Passenger
+        [ResponseType(typeof(Passenger))]
+        public IHttpActionResult PostPassenger(Passenger passenger)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Passenger.Add(passenger);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = passenger.ID }, passenger);
+        }
+
+        // DELETE: api/Passenger/5
+        [ResponseType(typeof(Passenger))]
+        public IHttpActionResult DeletePassenger(int id)
+        {
+            Passenger passenger = db.Passenger.Find(id);
+            if (passenger == null)
+            {
+                return NotFound();
+            }
+
+            db.Passenger.Remove(passenger);
+            db.SaveChanges();
+
+            return Ok(passenger);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PassengerExists(int id)
+        {
+            return db.Passenger.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ Testing Final Assignment/WebAPI/Models/Passenger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Passenger
+    {
+        public int ID { get; set; }
+
+        public Guid Number { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+    }
+
+    public class DBContext : DbContext
+    {
+        public DBContext() { }
+
+        public DbSet<Passenger> Passenger { get; set; }
+    }
+}
